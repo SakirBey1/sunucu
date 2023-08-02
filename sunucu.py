0711d17b8ce0c76194a6cd4bcaad0d48237e4409
SakirBey1 <sakirhack81@gmail.com>
2023-08-02 17:15:15 +0300

    Create sunucu.py

---
+++ sunucu.py
@@ -0,0 +1,8 @@
+print("Merhabalar Sunucuya Bağlanmak için yapınız.")
+kullanici_adi =input("Kullanıcı adı girin :")
+sifre = input("Kullanıcı şifresi girin :")
+
+if kullanici_adi=="sakirbeyy" and sifre=="qwertysakir123890":
+  print("⚡️Giriş Başarılı⚡️ \n ✨SakirBey Her türlü güvenceyi sağlamaktadır sunucuya başarıyla giriş yaptınız!✨\n✨PlayStoreye girip arama yerine 💎Wıre Tun💎 yazıp uygulamayı indirin💥\n💥Sonra Not Connecte yazısına bağlayın ve sunucumuzdan sizin telefondaki İP üzerinize internet verilecektir💥\n 🧑‍💻DeveloperBy: Sakir🧑‍💻")
+else:
+  print("Kullanıcı adı yada şifre hatalı")
